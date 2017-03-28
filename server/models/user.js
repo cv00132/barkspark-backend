a0386a8f37d583037d8a928f3be5b6aa324cfe2b
Cory Vaughan <codercory13@gmail.com>
2017-03-28 09:41:01 -0400

    Fixed getDogs method.

--- server/models/user.js
+++ server/models/user.js
@@ -13,7 +13,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
-        User.hasMany(models.Dog)
+        User.hasMany(models.Dog, { foreignKey:'userId' })
       }
     }
   });

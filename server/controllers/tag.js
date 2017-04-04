aaa0e20200d694f50184f73b45ac9719f7924fff
Cory Vaughan <codercory13@gmail.com>
2017-04-04 11:18:34 -0400

    Getting userId from the authenticated user.

--- server/controllers/tag.js
+++ server/controllers/tag.js
@@ -4,7 +4,8 @@ module.exports = {
 
     addTag (req, res) {
         Tag.create({
-            tag: req.body.tag
+            tag: req.body.tag,
+            userId: req.user.id
         })
         .then(tag => res.status(201).send(tag))
         .catch(error => res.status(400).send(error));

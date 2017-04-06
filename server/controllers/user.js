99202e0ff89648baea402328513a41c98132f2a8
Cory Vaughan <codercory13@gmail.com>
2017-04-06 09:51:08 -0400

    Added DOB back on to Home profile.

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -95,6 +95,7 @@ module.exports = {
                    'id',
                    'username',
                    'location',
+                   'DOB',
                    'description',
                    'profilePic'
                ]
